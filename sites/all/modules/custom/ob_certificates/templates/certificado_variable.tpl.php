34329b46d11c4ecea4dee45ee9e5590ca212d3f3
Sergio Vargas <sergio.svargass@gmail.com>
2014-04-02 01:12:04 -0500

    certificates

---
+++ sites/all/modules/custom/ob_certificates/templates/certificado_variable.tpl.php
@@ -0,0 +1,15 @@
+<h3>LABORATORIOS LA SANTE S.A.</h3>
+<h3>CERTIFICA QUE</h3>
+<p>La señora <?php print $personal['NOMBRE_COMPLETO'] ?> indentificada con cedula 
+de ciudadanía No. <?php print $personal['EMPLEADO'] ?>
+Labora en esta compañía desde el  <?php print $personal['dia'] ?> 
+de  <?php print $personal['mes'] ?> de  <?php print $personal['ano'] ?> con un contrato de trabajo a 
+_________________ ___________________ desempeñando en la actualidad el cargo de 
+<?php print $personal['NOMBRE_CARGO'] ?> con un salario básico menusal 
+<?php print $personal['salario_letr'] ?> (<?php print $personal['salario_num'] ?>). Y un salario promedio 
+variable de <?php print $personal['variable_letr'] ?> (<?php print $personal['variable_num'] ?>).
+</p>
+<p>
+La presente certificación se expide en la ciudad de Bogotá, a los <?php print $personal['act_dia'] ?>
+días del mes de <?php print $personal['act_mes'] ?> de <?php print $personal['act_ano'] ?>
+</p>
