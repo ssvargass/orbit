a5a234ff34f74998ead0ed94513620a43fabd1c0
Sergio Vargas <sergio.svargass@gmail.com>
2014-04-15 09:47:53 -0500

    cambios

---
+++ sites/all/modules/custom/ob_room/templates/author.tpl.php
@@ -0,0 +1,8 @@
+<strong>El evento <?php $node->title ?> se ha creado exitosamente.</strong>
+<br>
+<br>
+Inicio: <?php print $node->field_fecha['und'][0]['value']; ?>
+<br>
+Fin: <?php print $node->field_fecha['und'][0]['value2']; ?>
+<br>
+Descripción: <?php print $node->field_descripci_n['und']; ?>
