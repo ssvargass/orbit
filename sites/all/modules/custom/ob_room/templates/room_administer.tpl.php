14120ccd41f5fef53cf362f1d18f875b8169a3f4
Sergio Vargas <sergio.svargass@gmail.com>
2014-04-15 16:24:32 -0500

    Cambios

---
+++ sites/all/modules/custom/ob_room/templates/room_administer.tpl.php
@@ -0,0 +1,15 @@
+Se ha generado una nueva solicitud para la sala de reunión <?php print $node->sala->title?>
+por favor ingrese al administrador de salas en Orbit para ver la solicitud.
+
+Inicio: <?php print $node->field_fecha['und'][0]['value']; ?>
+<br>
+Fin: <?php print $node->field_fecha['und'][0]['value2']; ?>
+<br>
+Descripción: <?php print $node->field_descripci_n['und'][0]['value']; ?>
+<br>
+<?php if(count($node->invitados) > 0): ?>
+Invitados:
+<?php foreach ($node->invitados as $key => $value) { 
+  print $value->name;
+}?>
+<?php endif; ?>
---
+++ sites/all/modules/custom/ob_room/templates/room_create.tpl.php
@@ -0,0 +1,19 @@
+<strong>La solicitud <?php $node->title ?> se ha creado exitosamente.</strong>
+<br>
+<br>
+Inicio: <?php print $node->field_fecha['und'][0]['value']; ?>
+<br>
+Fin: <?php print $node->field_fecha['und'][0]['value2']; ?>
+<br>
+Descripción: <?php print $node->field_descripci_n['und'][0]['value']; ?>
+<br>
+<?php if(count($node->invitados) > 0): ?>
+Invitados:
+<?php foreach ($node->invitados as $key => $value) { 
+  print $value->name;
+}?>
+<?php endif; ?>
+<br>
+Es necesario que el web master realice la validación correctamente, 
+<br>
+una vez hecho este proceso, le llegará un nuevo correo.
